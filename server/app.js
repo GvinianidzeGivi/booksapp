00bc52c39f1130feafcb1452486db8106b3f0f71
gvinianidzegivi <gvinianidzegivi@gmail.com>
2021-06-05 17:20:06 +0300

    connection to database with mongoose

--- server/app.js
+++ server/app.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
+const mongoose = require('mongoose');
+
+require('dotenv').config()
 
 const app = express();
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@books.agsvd.mongodb.net/test`,
+{ useUnifiedTopology: true, useNewUrlParser: true })
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
     graphiql: true
 }));
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
- console.log(`Listening on ${port}`);
+ console.log(`Server is listening on port: ${port}`);
 });
+
