25cc6b8928a778a0c5904a303b836dfb17657ef6
gvinianidzegivi <gvinianidzegivi@gmail.com>
2021-06-06 21:56:11 +0400

    react cleanup, added booklist component

---
+++ client/src/components/BookList/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BookList = () => {
+  return (
+    <div>
+      <ul class="book-list">
+        <li>Book name:</li>
+      </ul>
+    </div>
+  );
+};
+
+export default BookList;
